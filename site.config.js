4924bef35db7206976ae0951256257084057e247
Juucci <162023828+Juucci@users.noreply.github.com>
2024-03-09 16:01:29 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Juu0113",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "AWS Developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    email: "wowll55@naver.com",
     linkedin: "morethanmin",
-    github: "morethanmin",
+    github: "Juucci",
     instagram: "",
   },
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/Juucci/morethan-log_Juu0113",
     },
   ],
   // blog setting (required)
